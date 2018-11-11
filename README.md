6eb8efa685da3e38d7053cdb8bf0e7253d60636c
Daniel Stieber <daniel.w.stieber@gmail.com>
2018-11-11 20:01:18 +0100

    0.0.1 initial commit

---
+++ README.md
@@ -0,0 +1,127 @@
+CodaPHP
+=======================
+![Coda API Version](https://img.shields.io/badge/Coda_API_version-v1beta1-orange.svg)
+
+CodaPHP is a library that makes it easy to use data from [Coda](https://www.coda.io) 
+docs your in web projects by using the [Coda API (Beta)](https://coda.io/developers/apis/v1beta1). 
+Coda itself as well as the API is still in beta. Use on your own risk.
+
+Easily use all available API calls with one library including
+* List all documents
+* Read data from tables, formulas and controls
+* Add/modify rows
+* and a lot more
+
+## Quickstart
+### Installation and basic usage
+Install the library through [Composer](http://getcomposer.org/):
+```bash
+php composer.phar require danielstieber/coda-php
+```
+and add it to your project:
+```PHP
+require './vendor/autoload.php';
+$coda = new CodaPHP('<YOUR API TOKEN>');
+
+// List all your docs
+$result = $coda->listDocs();
+var_dump($result);
+```
+
+### Handling table data
+Let's say we have the table 'Products' in our Coda doc:
+#### Products
+| Title   ⚑ | Price | Status      |
+|-----------|-------|-------------|
+| Goatmilk  | 14.90 | available ▼ |
+| Goatmeat  | 38.90 | available ▼ |
+
+```PHP
+// Get the price of the goatmilk
+$docId = $coda->getDocId('<YOUR DOC URL>');
+$result = $coda->getRow($docId, 'Products', 'Goatmilk');
+var_dump($result['values']['Price']);
+// Add the new product 'Goatcheese'
+if($coda->insertRows($docId, 'Products', ['Title' => 'Goatcheese', 'Price' => 23.50, 'Status' => 'available'])) {
+	echo 'Product added';
+}
+// Update the new status of 'Goatmilk'
+if($coda->insertRows($docId, 'Products', ['Title' => 'Goatmilk', 'Status' => 'sold out'], ['Title'])) {
+	echo 'Product upated';
+}
+```
+
+## Overview
+This is a personal side project. If you have any suggestions, find bugs or want to contribute, don't hesitate to contact me. You can use the [offical Coda community](https://community.coda.io/) to asks questions and reach out as well.
+
+### Token
+Generate your token in the Coda profile settings. *Notice: Everyone with this token has full access to all your docs!*
+
+### Methods
+The method names are inspired by the wording of the [official Coda API documentation](https://coda.io/developers/apis/v1beta1) and are listed below.
+
+### Parameters
+All parameters can be found in the [official Coda API documentation](https://coda.io/developers/apis/v1beta1). Just add an associativve array with your parameters to selected functions. The parameter _useColumnNames_ is set true by default in all 'row' functions. I list the important ones below.
+
+### Response
+In case of success, responses are mostly untouched but converted to PHP arrays. Exception is `insertRow()` function, which provides a boolean true in case of success.
+In case of an error, the response includes the statusCode and provided error message, also untouched and converted to an array.
+
+## Documentation
+```PHP
+$coda = new CodaPHP('<YOUR API TOKEN>'); // Create instance of CodaPHP
+```
+### Docs
+```PHP
+$coda->getDocId('<YOUR DOC URL>'); // Get the id of a doc
+
+$coda->listDocs(); // List all docs you have access to
+$coda->listDocs(['query' => 'todo']);  // List docs filtered by searchquery 'todo'
+$coda->getDoc('<DOC ID>'); // Get a specific doc
+$coda->createDoc('My new doc'); // Create a new doc
+$coda->createDoc('Copy of my old doc', '<DOC ID>'); // Copy a doc
+```
+### Folders and Sections
+```PHP
+$coda->listSections('<DOC ID>'); // List all sections in a doc
+$coda->getSection('<DOC ID>', '<SECTION NAME OR ID>'); // Get a section in a doc
+
+$coda->listFolders('<DOC ID>'); // List all folders in a doc
+$coda->getFolder('<DOC ID>', '<FOLDER NAME OR ID>'); //Get a folder in a doc
+```
+### Tables, columns and rows
+```PHP
+$coda->listTables('<DOC ID>'); // List all tables in a doc
+$coda->getTable('<DOC ID>', '<TABLE NAME OR ID>'); // Get a table in a doc
+
+$coda->listColumns('<DOC ID>', '<TABLE NAME OR ID>'); // List all columns in a table
+$coda->getColumn('<DOC ID>', '<TABLE NAME OR ID>', '<COLUMN NAME OR ID>'); // Get a column in a table
+
+$coda->listRows('<DOC ID>', '<TABLE NAME OR ID>'); // List all row in a table
+$coda->insertRows('<DOC ID>', '<TABLE NAME OR ID>', [['<COLUMN ID OR NAME>' => '<VALUE>']], ['<KEYCOLUMN>']); // Insert/updates a row in a table
+
+// Examples:
+$coda->insertRows('<DOC ID>', 'todos', ['title' => 'Shower']); // Adds one row to 'todo'
+$coda->insertRows('<DOC ID>', 'todos', [['title' => 'Wash dishes'], ['title' => 'Clean car']]); // Adds two row to 'todo'
+$coda->insertRows('<DOC ID>', 'todos', [['title' => 'Shower', 'status' => 'done'], ['title' => 'Buy goatcheese']], ['title']); // Updates the status of 'Shower' and inserts a new todo
+
+$coda->updateRow('<DOC ID>', '<TABLE NAME OR ID>', '<ROW NAME OR ID>', ['<COLUMN ID OR NAME>' => '<VALUE>']); // List all row in a table
+$coda->getRow('<DOC ID>', '<TABLE NAME OR ID>', '<ROW NAME OR ID>'); // Get a row in a table
+$coda->deleteRow('<DOC ID>', '<TABLE NAME OR ID>', '<ROW NAME OR ID>'); // Deletes a row in a table
+```
+### Formulas and Controls
+```PHP
+$coda->listFormulas('<DOC ID>'); // List all formulas in a doc
+$coda->getFormula('<DOC ID>', '<FORMULA NAME OR ID>'); // Get a formula in a doc
+
+$coda->listControls('<DOC ID>'); // List all controls in a doc
+$coda->getControl('<DOC ID>', '<CONTROL NAME OR ID>'); //Get a control in a doc
+```
+### Account
+```PHP
+$coda->whoAmI(); // Get information about the current account
+```
+
+## Changelog
+### 0.0.1 (November 11, 2018)
+* Initial version based on version1beta1 of Coda API
---
+++ src/CodaPHP/CodaPHP.php
@@ -0,0 +1,384 @@
+<?php
+namespace CodaPHP;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+
+/**
+ * CodaPHP Library
+ * 
+ * CodaPHP is a library that makes it easy to use data from Coda (https://www.coda.io)
+ * docs your in web projects by using the Coda API (Beta) (https://coda.io/developers/apis/v1beta1).
+ * Coda itself as well as the API is still in beta. Use on your own risk.
+ * 
+ * This file is licensed under the MIT license.
+ */
+class CodaPHP
+{
+	/**
+	 * The API Token and the guzzle client
+	 * @var string $apiToken
+	 * @var obj $client
+	 */
+	protected $apiToken, $client;
+
+	/**
+	 * Base URL of the Coda Api
+	 */
+	const API_BASE = 'https://coda.io/apis/v1beta1';
+
+	/**
+	 * Creates new guzzle client with authentication
+	 */
+	public function __construct($apiToken)
+	{
+		$this->apiToken = $apiToken;
+		$this->client = new Client([
+			'headers' => [
+				'Authorization' => 'Bearer '.$this->apiToken
+			]
+		]);
+	}
+	/**
+	 * Performs a request using guzzle
+	 * 
+	 * @param string $url
+	 * @param array $params Guzzle request params
+	 * @param string $method HTTP request method
+	 * @param bool $addstatus When true, the return will include the HTTP status code
+	 * @return array
+	 */
+	protected function request($url, array $params = [], $method = 'GET', $addStatus = false)
+	{
+		try {
+			$response = $this->client->request($method, self::API_BASE . $url, $params);
+		} catch (BadResponseException $e) {
+			$errorContent = $e->getResponse()->getBody()->getContents();
+			return(json_decode($errorContent, true));
+		}
+		$httpCode = $response->getStatusCode();
+		$responseString = $response->getBody()->getContents();
+		$dataArray = json_decode($responseString, true);
+
+		if (is_array($dataArray) && JSON_ERROR_NONE === json_last_error()) {
+			if($addStatus) {
+				return ['statusCode' => $httpCode, 'result' => $dataArray];
+			} else {
+				return $dataArray;
+			}
+		}
+		throw new \UnexpectedValueException('Invalid JSON: ' . $responseString);
+	}
+	/**
+	 * Returns the Coda doc id of a Coda doc url 
+	 * 
+	 * @param string $url Coda doc url
+	 * @return string 10-digit Coda doc id
+	 */
+	public static function getDocId($url) {
+		$re = '/coda.io\/d\/.*?_d(.{10})\//m';
+		preg_match($re, $url, $res);
+		return $res[1] ?? false;
+	}
+	/**
+	 * Returns infos about the user
+	 * 
+	 * @return array 
+	 */
+	public function whoAmI()
+	{
+		$res = $this->request('/whoami');
+		return $res;
+	}
+	/**
+	 * Returns an array of docs
+	 * 
+	 * @param array $params Optional query parameters listed here https://coda.io/developers/apis/v1beta1#operation/listDocs
+	 * @return array 
+	 */
+	public function listDocs(array $params = [])
+	{
+		$res = $this->request('/docs?'.http_build_query($params));
+		return $res;
+	}
+	/**
+	 * Returns information of a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @return array 
+	 */
+	public function getDoc($doc)
+	{
+		$res = $this->request('/docs/'.$doc);
+		return $res;
+	}
+	/**
+	 * Creates a new doc
+	 * 
+	 * @param string $title Name of the doc
+	 * @param string $source Id of a doc to use as source (creates a copy)
+	 * @return array 
+	 */
+	public function createDoc($title = '', $source = '')
+	{
+		$params['title'] = $title;
+		$params['sourceDoc'] = $source;
+		$res = $this->request('/docs', ['json' => $params], 'POST');
+		return $res;
+	}
+	/**
+	 * Returns sections in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param array $params Optional query parameters listed here https://coda.io/developers/apis/v1beta1#operation/listSections
+	 * @return array
+	 */
+	public function listSections($doc, array $params = [])
+	{
+		$res = $this->request('/docs/'.$doc.'/sections?'.http_build_query($params));
+		return $res;
+	}
+	/**
+	 * Returns a section in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $section Id or name of a section
+	 * @return array
+	 */
+	public function getSection($doc, $sesectionc)
+	{
+		$res = $this->request('/docs/'.$doc.'/sections/'.$section);
+		return $res;
+	}
+	/**
+	 * Returns folders in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param array $params Optional query parameters listed here https://coda.io/developers/apis/v1beta1#operation/listFolders
+	 * @return array
+	 */
+	public function listFolders($doc, array $params = [])
+	{
+		$res = $this->request('/docs/'.$doc.'/folders?'.http_build_query($params));
+		return $res;
+	}
+	/**
+	 * Returns a folder in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $folder Id or name of a folder
+	 * @return array
+	 */
+	public function getFolder($doc, $folder)
+	{
+		$res = $this->request('/docs/'.$doc.'/folders/'.$folder);
+		return $res;
+	}
+	/**
+	 * Returns tables in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param array $params Optional query parameters listed here https://coda.io/developers/apis/v1beta1#operation/listTables
+	 * @return array
+	 */
+	public function listTables($doc, array $params = [])
+	{
+		$res = $this->request('/docs/'.$doc.'/tables?'.http_build_query($params));
+		return $res;
+	}
+	/**
+	 * Returns a table in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $table Id or name of a table
+	 * @return array
+	 */
+	public function getTable($doc, $table) 
+	{
+		$res = $this->request('/docs/'.$doc.'/tables/'.$table);
+		return $res;
+	}
+	/**
+	 * Returns columns in a table
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $table Id or name of a table
+	 * @param array $params Optional query parameters listed here https://coda.io/developers/apis/v1beta1#operation/listColumns
+	 * @return array
+	 */
+	public function listColumns($doc, $table, array $params = [])
+	{
+		$res = $this->request('/docs/'.$doc.'/tables/'.$table.'/columns?'.http_build_query($params));
+		return $res;
+	}
+	/**
+	 * Returns a column in a table
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $table Id or name of a table
+	 * @param string $column Id or name of a column
+	 * @return array
+	 */
+	public function getColumn($doc, $table, $column)
+	{
+		$res = $this->request('/docs/'.$doc.'/tables/'.$table.'/columns/'.$column);
+		return $res;
+	}
+	/**
+	 * Returns rows in a table
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $table Id or name of a table
+	 * @param array $params Optional query parameters listed here https://coda.io/developers/apis/v1beta1#operation/listRows , useColumnNames is set true by default
+	 * @return array
+	 */
+	public function listRows($doc, $table, array $params = [])
+	{
+		$params['useColumnNames'] = $params['useColumnNames'] ?? true; 
+		$res = $this->request('/docs/'.$doc.'/tables/'.$table.'/rows?'.http_build_query($params));
+		return $res;
+	}
+	/**
+	 * Inserts or updates a row in a table
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $table Id or name of a table
+	 * @param array $rowData Associative array with your row data. Can be one row as array or an array of mulitple rows as an array (arrayception). Keys has to be column ids or names.
+	 * @param array $keyColumns Array with ids or names of columns. Coda will update rows instead of inserting, if keyColumns are matching
+	 * @return bool
+	 */
+	public function insertRows($doc, $table, array $rowData, array $keyColumns = [])
+	{
+		if($this->countDimension($rowData) == 1)
+			$rowData = [$rowData]; 
+		$i = 0;
+		foreach($rowData as $row) {
+			foreach($row as $column => $value) {
+				$params['rows'][$i]['cells'][] = ['column' => $column, 'value' => $value];
+			}
+			$i++;
+		}
+		$params['keyColumns'] = $keyColumns;
+		$res = $this->request('/docs/'.$doc.'/tables/'.$table.'/rows', ['json' => $params], 'POST', true);
+		if($res['statusCode'] === 202) {
+			return true;
+		} else {
+			return $res;
+		}
+	}
+	/**
+	 * Returns a row in a table
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $table Id or name of a table
+	 * @param string $row Id or name of a column
+	 * @param array $params Optional query parameters listed here https://coda.io/developers/apis/v1beta1#operation/getRow , useColumnNames is set true by default
+	 * @return array
+	 */
+	public function getRow($doc, $table, $row, array $params = [])
+	{
+		$params['useColumnNames'] = $params['useColumnNames'] ?? true; 
+		$res = $this->request('/docs/'.$doc.'/tables/'.$table.'/rows/'.$row.'?'.http_build_query($params));
+		return $res;
+	}
+	/**
+	 * Updates a row in a table
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $table Id or name of a table
+	 * @param string $row Id or name of a row
+	 * @param array $rowData Associative array with your row data
+	 * @return string Id of the updated row
+	 */
+	public function updateRow($doc, $table, $row, array $rowData)
+	{
+		foreach($rowData as $column => $value) {
+			$params['row']['cells'][] = ['column' => $column, 'value' => $value];
+		}
+		$res = $this->request('/docs/'.$doc.'/tables/'.$table.'/rows/'.$row, ['json' => $params], 'PUT');
+		return $res;
+	}
+	/**
+	 * Deltes a row in a table
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $table Id or name of a table
+	 * @param string $row Id or name of a row
+	 * @return string Id of the deleted row
+	 */
+	public function deleteRow($doc, $table, $row)
+	{
+		$res = $this->request('/docs/'.$doc.'/tables/'.$table.'/rows/'.$row, [], 'DELETE');
+		return $res;
+	}
+	/**
+	 * Returns formulas in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param array $params Optional query parameters listed here https://coda.io/developers/apis/v1beta1#operation/listFormulas
+	 * @return array
+	 */
+	public function listFormulas($doc, array $params = [])
+	{
+		$res = $this->request('/docs/'.$doc.'/formulas?'.http_build_query($params));
+		return $res;
+	}
+	/**
+	 * Returns a formula in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $formula Id or name of a formula
+	 * @return array
+	 */
+	public function getFormula($doc, $formula)
+	{
+		$res = $this->request('/docs/'.$doc.'/formulas/'.$formula);
+		return $res;
+	}
+	/**
+	 * Returns controls in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param array $params Optional query parameters listed here https://coda.io/developers/apis/v1beta1#operation/listControls
+	 * @return array
+	 */
+	public function listControls($doc, array $params = [])
+	{
+		$res = $this->request('/docs/'.$doc.'/controls?'.http_build_query($params));
+		return $res;
+	}
+	/**
+	 * Returns a control in a doc
+	 * 
+	 * @param string $doc Id of a doc
+	 * @param string $control Id or name of a control
+	 * @return array
+	 */
+	public function getControl($doc, $control)
+	{
+		$res = $this->request('/docs/'.$doc.'/controls/'.$control);
+		return $res;
+	}
+	/**
+	 * Resolves a link
+	 * 
+	 * @param string $url Url of a doc
+	 * @return array
+	 */
+	public function resolveLink($url)
+	{
+		$res = $this->request('/resolveBrowserLink?url='.$url);
+		return $res;
+	}
+	/**
+	 * Counts dimensions of an array
+	 * 
+	 * @param array $array
+	 * @return int
+	 */
+	protected function countDimension($array)
+	{
+		if (is_array(reset($array))) { $return = $this->countDimension(reset($array)) + 1; } else { $return = 1; }
+		return $return;
+	} 
+}
